5ae2deec2f3f3fdf42a344a1c94dfdcd01e4aa15
bilal-salsoft <bilal.razzak@salsoft.co>
2021-03-30 19:46:47 +0500

    api fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,46 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+
+    public function demo_mail(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fullName'      => 'required|',
+            'email'     => 'required|email',
+            'subject'   => 'required',
+            'message'   => 'required',
+        ], [
+            'fullName.required'     => 'Please provide name',
+            'email.required'         => 'Please provide email',
+            'email.email'       => 'Please provide valid email',
+            'subject.required'  => 'Please provide subject',
+            'message.required'  => 'Please provide message'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'error' => $validator->errors()->messages(),
+                    'msg' => 'Please fill all the required fields.'
+                ],
+                401
+            );
+        }
+
+
+        Mail::raw("Name: " . $request->fullName . "\nMessage: " . $request->message . "\nEmail: " . $request->email, function ($message) use ($request) {
+            $message->to('support@majesticares.com')->subject("Contact Us (" . $request->subject . ")");
+        });
+
+        Mail::raw("Name: " . $request->fullName . "\nMessage: " . $request->message . "\nEmail: " . $request->email, function ($message) use ($request) {
+            $message->to('maxwilson908@gmail.com')->subject("Contact Us (" . $request->subject . ")");
+        });
+
+        Mail::raw("Thank you contacting us. We will get back to you soon", function ($message) use ($request) {
+            $message->to($request->email)->subject("Contact us (support@majesticares.com) ");
+        });
+        return \response()->json(["message" => "Your message has been sent."], 200);
     }
 }
