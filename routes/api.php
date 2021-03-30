137359f86bba712b3b1f4279de31d3770c19e080
bilal-salsoft <bilal.razzak@salsoft.co>
2021-03-30 19:51:54 +0500

    api fixes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,44 @@ use Illuminate\Support\Facades\Validator;
 
 //public testing api
 
-Route::post('/contactUs_demo', 'App\Http\Controllers\HomeController@demo_mail');
+Route::post('/contactUs_demo', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'fullName'      => 'required|',
+        'email'     => 'required|email',
+        'subject'   => 'required',
+        'message'   => 'required',
+    ], [
+        'fullName.required'     => 'Please provide name',
+        'email.required'         => 'Please provide email',
+        'email.email'       => 'Please provide valid email',
+        'subject.required'  => 'Please provide subject',
+        'message.required'  => 'Please provide message'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(
+            [
+                'error' => $validator->errors()->messages(),
+                'msg' => 'Please fill all the required fields.'
+            ],
+            401
+        );
+    }
+
+
+    Mail::raw("Name: " . $request->fullName . "\nMessage: " . $request->message . "\nEmail: " . $request->email, function ($message) use ($request) {
+        $message->to('support@majesticares.com')->subject("Contact Us (" . $request->subject . ")");
+    });
+
+    Mail::raw("Name: " . $request->fullName . "\nMessage: " . $request->message . "\nEmail: " . $request->email, function ($message) use ($request) {
+        $message->to('maxwilson908@gmail.com')->subject("Contact Us (" . $request->subject . ")");
+    });
+
+    Mail::raw("Thank you contacting us. We will get back to you soon", function ($message) use ($request) {
+        $message->to($request->email)->subject("Contact us (support@majesticares.com) ");
+    });
+    return \response()->json(["message" => "Your message has been sent."], 200);
+});
 
 //
 
